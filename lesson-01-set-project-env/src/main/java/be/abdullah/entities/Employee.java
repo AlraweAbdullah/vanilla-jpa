65f9b02357056a9fdd07cb13afe03270d57baccd
Abdullah Alrawe <a14alrawi@gmail.com>
2026-09-24 04:53:49 +0200

    Change folders structure

--- lesson-01-set-project-env/src/main/java/be/abdullah/entities/Employee.java
+++ lesson-01-set-project-env/src/main/java/be/abdullah/entities/Employee.java
@@ -1,11 +1,15 @@
-package entities;
+package be.abdullah.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
